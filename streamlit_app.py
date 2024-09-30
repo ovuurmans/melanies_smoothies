5ce739d7009c974a6ca51a13e79a5232913b56af
ovuurmans <92381886+ovuurmans@users.noreply.github.com>
2024-09-30 16:56:17 +0200

    Update streamlit_app.py
    move fv_df in block

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,9 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
     #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -53,8 +56,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}')
-
-# New section to display fruityvide information
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
