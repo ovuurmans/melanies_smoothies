8f2f7c97477d3874596f188a6a6774167f021538
ovuurmans <92381886+ovuurmans@users.noreply.github.com>
2024-09-30 16:53:01 +0200

    Update streamlit_app.py
    add fv_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,4 +56,5 @@ if ingredients_list:
 
 # New section to display fruityvide information
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
